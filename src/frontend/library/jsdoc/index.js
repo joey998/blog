652e8f455d4678edcbeee1a9798bb0f0f5382351
tedjmzhang <2273243363@qq.com>
2024-06-03 21:22:38 +0800

    add jsdoc

---
+++ src/frontend/library/jsdoc/index.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+function Book(title, author) {
+  console.log(title, author)
+}
+
+module.exports = {
+  Book
+}
